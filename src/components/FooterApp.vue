23c6ed99ab3135aba43db650fd106152c96855fc
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-15 17:40:26 +0200

    upper footer structur

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,7 +1,55 @@
 <template>
-    <div>
-        
-    </div>
+    <footer>
+        <section class="upper_footer">
+            <div class="listes">
+                <div class="list">
+                    <h2>Dc comics</h2>
+                    <ul>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <h2>Dc</h2>
+                    <ul>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <h2>Sites</h2>
+                    <ul>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <h2>Shop</h2>
+                    <ul>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                        <li>contenuco</li>
+                    </ul>
+                </div>
+              
+            </div>
+            <div>
+                <img src="../assets/img/dc-logo-bg.png" alt="">
+            </div>
+        </section>
+        <section class="lower_footer"></section>
+    </footer>
 </template>
 
 <script>
@@ -17,5 +65,23 @@ export default {
 
 <!-- lang="scss" -->
 <style scoped>
-
+section.upper_footer{
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    background: url(../assets/img/footer-bg.jpg);
+}
+div.listes{
+    display: flex;
+}
+h2{
+    color: white;
+}
+ul{
+    list-style-type: none;
+    padding-left: 0;
+}
+ul li{
+    color: white;
+}
 </style>
